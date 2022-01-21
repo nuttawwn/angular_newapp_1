f4d182948589dc8a3a93a6400d598a5a079beaa2
nuttawwn <nuttawutok111@gmail.com>
2022-01-22 01:23:51 +0700

    add code in brach

---
+++ src/app/action-bar/action-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+@Component({
+  selector: 'app-action-bar',
+  templateUrl: './action-bar.component.html',
+  styleUrls: ['./action-bar.component.scss']
+})
+export class ActionBarComponent implements OnInit {
+ @Input() add_num : number = 1; // เป็นการรับ input ในหนั้า HtmL (action-bar.component) = [add_num]
+ @Input() numre : number = 0;
+ @Output() numberChange = new EventEmitter
+ //การเลือกใช้ EventEmiter ในการทำ output Binding ให้เลือกเฉพาะ @angular/core เท่านั้น
+  num: number = 0
+  
+  increaseNum() {
+    console.log('"this.increaseNum()"');    
+    if (this.num >0) {
+      this.num = this.num - this.add_num 
+      this.numberChange.emit(this.num); // เป็นการส่ง paramater ออกไปพร้อมกับ output Event
+    }
+  }
+  decreaseNum() {
+    console.log("this.decreaseNum");   
+    if(this.num <100){
+    this.num = this.num + this.add_num;
+    this.numberChange.emit(this.num);
+    }
+  }
+  constructor() { }
+  ngOnInit(){
+    this.num = this.numre
+    console.log("this.num action bar>>>>>>>>>>",this.num)
+    
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_newapp_1';
+  testEventBind() {
+    console.log("test Evenbinding click");
+  }
+  testNumberChange(value : number) {
+    console.log("test NumberChange from app action bar : ");
+    console.log("this.value >>>>>>",value)
+  }
+  resetValue(numre : number)
+  {
+     console.log("resetNNumber",numre);
+     
+     
+  }
+
+  // (numberChange) = "testNumberChange()"
+  // tesyNumberChange = method สำหรับ output EventBinding ของ app action bar
+  //  (numberChange) = output ในตัวapp action bar
+
+  //ค่าตัวแปนที่ส่งจาก output componant นั้น จะต่่องมีค่าสัญลักษณ์ว่า $event
 }
