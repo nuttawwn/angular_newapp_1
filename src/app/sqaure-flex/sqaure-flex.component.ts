c08ae3f9021fb5fa88598026b7edd97359017cdb
nuttawwn <nuttawutok111@gmail.com>
2022-01-24 22:20:49 +0700

    add input

--- src/app/sqaure-flex/sqaure-flex.component.ts
+++ src/app/sqaure-flex/sqaure-flex.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sqaure-flex',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sqaure-flex.component.scss']
 })
 export class SqaureFlexComponent implements OnInit {
-
+  
+  @Input() divWidth = 500;
+  @Input() divHight = 200;
   constructor() { }
 
   ngOnInit(): void {
