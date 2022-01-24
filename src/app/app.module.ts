f0d3170f31cb6c4ab8213124e5af995e27044880
nuttawwn <nuttawutok111@gmail.com>
2022-01-24 22:20:04 +0700

    add sqaure-flex component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ActionBarComponent } from './action-bar/action-bar.component';
 import { MaxMinMeterComponent } from './max-min-meter/max-min-meter.component';
+import { SqaureFlexComponent } from './sqaure-flex/sqaure-flex.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ActionBarComponent,
-    MaxMinMeterComponent
+    MaxMinMeterComponent,
+    SqaureFlexComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sqaure-flex/sqaure-flex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sqaure-flex',
+  templateUrl: './sqaure-flex.component.html',
+  styleUrls: ['./sqaure-flex.component.scss']
+})
+export class SqaureFlexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
