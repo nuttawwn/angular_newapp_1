6d9c3d4fda0775805a474512514c0c17b210b94c
nuttawwn <nuttawutok111@gmail.com>
2022-01-30 16:31:13 +0700

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -16,7 +17,8 @@ import { RxMatSliderComponent } from './rx-mat-slider/rx-mat-slider.component';
     RxMatSliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
