c7ed5f7a3773354bebc754a59970f732e5138919
nuttawwn <nuttawutok111@gmail.com>
2022-01-30 17:33:51 +0700

    prictice api get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ImageUploadPageComponent } from './image-upload-page/image-upload-page.component';
 import { MaterialTestComponent } from './material-test/material-test.component';
 import { RxMatSliderComponent } from './rx-mat-slider/rx-mat-slider.component';
+import { TestRequestModule } from './test-request/test-request.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RxMatSliderComponent, 
     CardComponent,
     ImageUploadPageComponent,
     MaterialTestComponent,
-    RxMatSliderComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    TestRequestModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test-request/test-request-get/test-request-get.component.ts
+++ src/app/test-request/test-request-get/test-request-get.component.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { Book } from './test-request-get';
 @Component({
   selector: 'app-test-request-get',
   templateUrl: './test-request-get.component.html',
   styleUrls: ['./test-request-get.component.css']
 })
 export class TestRequestGetComponent implements OnInit {
-
-  constructor() { }
-
+  bookList: Book[] = [];
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    // this.http.get<Book[]>('https://www.anapioficeandfire.com/api/books')
+    // .subscribe(response => {
+    //   this.bookList = response;
+    // })
   }
-
+  searchBookList(name: string) {
+    this.http.get<Book[]>('https://www.anapioficeandfire.com/api/books?name='+name)
+      .subscribe(response => {
+        this.bookList = response;
+      })
+  }
 }
