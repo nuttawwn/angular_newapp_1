c946d1ea2d01cb1ea67e2039907c0a6f4e970387
nuttawwn <nuttawutok111@gmail.com>
2022-01-31 19:21:01 +0700

    add primeNg in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,101 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_newapp_1';
+  value2 : string = 'Hello Taoswagboy'
+
+  cars =
+  [
+    {
+      vin : 12,
+      year : 1990,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 13,
+      year : 1991,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 14,
+      year : 1992,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 15,
+      year : 1993,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 16,
+      year : 1994,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 17,
+      year : 1995,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 18,
+      year : 1996,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 19,
+      year : 1997,
+      brand : 'Honda',
+      color : 'red'
+
+    },
+    {
+      vin : 20,
+      year : 1998,
+      brand : 'Honda',
+      color : 'red'
+
+    }
+  ];
+  
+
+  // //get function ต้องมี retrun ด้วย
+  // get productPrice() 
+  // {
+  //   return 150;
+  // }
+  // constructor()
+  // {
+  //   console.log("this.productPrice>>>>>>>>>>>>>>",this.productPrice)
+  // }
+
 }
+
+
+//วิธีใช้ primeng 
+//1.yarn add primeng
+//2.yarn add primeicons
+//3.copy flie style 
+// - "node_modules/primeicons/primeicons.css"
+// - "node_modules/primeng/resources/themes/lara-light-indigo/theme.css"
+// - "node_modules/primeng/resources/primeng.min.css"
+// 4.นำไป copy to angular.json (property style add เพิ่ม)
+
+
+//วิธีใช้ table Component
+//1.ใส่คำสั่ง yarn add @angular/cdk
+//2.สามารถ import เรียกใช่ import component ใน primeNg ได้ ()
+// Ex import {TableModule} from 'primeng/table';
+// นำ TableModule ไป ใส่ใน import
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {InputTextModule} from 'primeng/inputtext';
+import {TableModule} from 'primeng/table';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
