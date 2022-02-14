880a531d128e427a9fe17e81682f1ad877e31161
nuttawwn <nuttawutok111@gmail.com>
2022-02-14 07:46:49 +0700

    set ui app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,16 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_newapp_1';
-  employeeCode = "007";
-  employeeName = "TAO"
-
-
-
-  
+  // employeeCode = "007";
+  // employeeName = "TAO"
+  bmi = 0;
+  info = "";
+  //เป็นตัวแปลที่เอาไว้เชื่อข้อมูลฝั่ง Html มายัง Typescript
+  weightForm = new FormControl(null, Validators.required); // null คือค้่าเริ่มต้น ของตัวแปรที่กำหนดเอาไว้
+  heightForm = new FormControl(null, Validators.required);
+  calcualte() {
+    if (this.weightForm.invalid || this.heightForm.invalid) {
+      return;
+    }
+    const weight = this.weightForm.value
+    const height = this.heightForm.value
+    console.log("const weight>>>>>>", weight);
+    console.log("const height>>>>>>", height);
+    this.bmi = weight / (height ** 2)
+  }
 }
-
-
-// (ngModelChange) เป้น out-put 
+// (ngModelChange) เป้น out-put
 // [ngModel] เป็น INput
 //[(ngModel)] เป็น Two way-binding
 // required directive เพื่อทำ validable
